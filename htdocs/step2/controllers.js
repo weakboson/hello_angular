d03d572824c90bf89b6395078a8db34c94757c8e
weakboson <weakboson@gmail.com>
2013-06-30 02:31:29 +0900

    add like button

--- htdocs/step2/controllers.js
+++ htdocs/step2/controllers.js
@@ -1,5 +1,11 @@
 function ColorListCtrl($scope, $http) {
   $http.get('colors.json').success(function(data) {
+    for(var i=0;i<data.length;i++){data[i].like=0;}
     $scope.colors = data;
   });
+}
+function ColorListItemCtrl($scope){
+  $scope.increment=function(){
+    $scope.color.like++;
+  }
 }
