2fe262e0050a6e16bb72bc533cd0436e58e0edcf
weakboson <weakboson@gmail.com>
2013-06-30 02:06:50 +0900

    remove comment

--- nws.js
+++ nws.js
@@ -1,10 +1,6 @@
 var nws = require('node-web-server');
-// 起動 (__dirname を current directory として localhost:8080 で起動)
 nws.run({
   host: "localhost",
   port: 8080,
   docRoot: "htdocs"
 }, __dirname);
-console.log(__dirname);
-// 10秒後に停止
-// setTimeout(nws.stop, 10000);
