f334212af228cf1f77d5f0160f8b993265df6897
weakboson <weakboson@gmail.com>
2013-06-30 01:20:40 +0900

    add .gitignore

---
+++ step2/controllers.js
@@ -0,0 +1,7 @@
+function ColorListCtrl($scope) {
+  $scope.colors = [
+    {"name":"桜色","kana":"さくらいろ","color":"#fef4f4"},
+    {"name":"黄金","kana":"こがね","color":"#e6b422"},
+    {"name":"櫨染","kana":"はじぞめ","color":"#d9a62e"}
+  ];
+}
